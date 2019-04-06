0db1172b741fe3f0893fc654be2eafa882f210b5
Carlos Lantigua <clantigua954@gmail.com>
2019-04-06 18:34:13 -0400

    simplified Modes code a bit

--- src/components/top/Modes.js
+++ src/components/top/Modes.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useContext } from 'react'
 import { Store } from '../../store/store'
 import { toBinaryString, hexString, octString } from '../utils/'
 import styled from '@emotion/styled'
@@ -12,7 +12,7 @@ const Modes = () => {
 
     return (
         <Container>
-            <p>HEX: {newHex.split('.')[0]}</p>
+            <p>HEX: {newHex}</p>
             <p>DEC: {input}</p>
             <p>OCT: {newOct}</p>
             <p>BIN: {newInput}</p>
@@ -24,11 +24,14 @@ export default Modes
 
 const Container = styled.div`
     height: 20px;
+    margin-bottom: 15px;
     p {
         display: flex;
         height: 100%;
         flex-direction: column;
         justify-content: space-between;
         letter-spacing: 3px;
+        font-size: 1.4rem;
+        margin-bottom: 5px;
         }
 `
